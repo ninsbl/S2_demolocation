8939dbdd8243bcc38e919276c35f78f02212939e
Stefan Blumentrath <stefan.blumentrath@gmx.de>
2021-09-04 23:57:14 +0200

    Create setup_grass.py

---
+++ setup_grass.py
@@ -0,0 +1,5 @@
+
+
+gscript.run_command("g.extension", url="https://github.com/ninsbl/m.crawl.thredds.git", extension="m.crawl.thredds")
+gscript.run_command("g.extension", url="https://github.com/ninsbl/t.rast.import.netcdf.git", extension="t.rast.import.netcdf")
+
