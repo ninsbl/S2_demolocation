0286f6af6375fece8914cfd961943bc17c22208b
Stefan Blumentrath <stefan.blumentrath@nina.no>
2021-09-06 00:42:59 +0200

    update

--- build_strds.py
+++ build_strds.py
@@ -1,4 +1,6 @@
+#!/usr/bin/env python3
+
 gscript.run_command("g.region", res=10, flags="a")
 for instr in ["A", "B"]:
-    for proj in ["T29", "T30", "T31", "T32", "T33", "T34", "T35"]:
-        gscript.run_command("t.rast.import.netcdf", input=f"S2{instr}_{proj}.txt", output="Sentinel_2_NBS", bandref="", flags="loa", nprocs=10)
+    for proj in ["T31", "T32", "T33", "T34", "T35"]:
+        gscript.run_command("t.rast.import.netcdf", input=f"S2{instr}_{proj}.txt", output="Sentinel_2", bandref="bandref.txt", flags="la{}".format("o" if proj == "T33" else ""), nodata="-1,65535", nprocs=20)
