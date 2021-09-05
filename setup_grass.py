935247a7497ab01a388831df6dd7d463c785e018
Stefan Blumentrath <stefan.blumentrath@nina.no>
2021-09-06 00:41:09 +0200

    add shebang, fix URL

--- setup_grass.py
+++ setup_grass.py
@@ -1,5 +1,5 @@
+#!/usr/bin/env python3
 
-
-gscript.run_command("g.extension", url="https://github.com/ninsbl/m.crawl.thredds.git", extension="m.crawl.thredds")
-gscript.run_command("g.extension", url="https://github.com/ninsbl/t.rast.import.netcdf.git", extension="t.rast.import.netcdf")
+gscript.run_command("g.extension", url="https://github.com/ninsbl/m.crawl.thredds/archive/refs/heads/master.zip", extension="m.crawl.thredds")
+gscript.run_command("g.extension", url="https://github.com/ninsbl/t.rast.import.netcdf/archive/refs/heads/master.zip", extension="t.rast.import.netcdf")
 
