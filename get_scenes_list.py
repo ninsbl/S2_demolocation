5a2e57ad1ac9c65a1d2ad50c5a10bce691d4448a
Stefan Blumentrath <stefan.blumentrath@nina.no>
2021-09-06 18:19:10 +0200

    split

--- get_scenes_list.py
+++ get_scenes_list.py
@@ -2,21 +2,23 @@
 
 from pathlib import Path
 import grass.script as gscript
+import os
 
 directory = Path("./scene_lists")
 
 if not directory.exists():
     directory.mkdir()
 
-for instr in ["A", "B"]:
-    print(f"Sentinel-2{instr}")
-    for year in [2021]:
-        print(f"{year}")
-        for proj in ["T31", "T32", "T33", "T34", "T35"]:
-            print(f"{proj}")
-            scenes = gscript.read_command("m.crawl.thredds", 
-                                          input=f"https://nbstds.met.no/thredds/catalog/NBS/S2{instr}/{year}/catalog.html",
-                                          output=directory.joinpath(f"S2{instr}_{year}_{proj}.txt"),
-                                          filter=f".*{proj}.*DTERRENG.*",
-                                          nprocs=10)
+instr = os.environ["S2_NSTRUMENT"]
+proj = os.environ["S2_PROJ"]
+year = os.environ["S2_YEAR"]
+
+print(f"Sentinel-2{instr}")
+print(f"{proj}")
+print(f"{year}")
+scenes = gscript.read_command("m.crawl.thredds", 
+                              input=f"https://nbstds.met.no/thredds/catalog/NBS/S2{instr}/{year}/catalog.html",
+                              output=directory.joinpath(f"S2{instr}_{year}_{proj}.txt"),
+                              filter=f".*{proj}.*DTERRENG.*",
+                              nprocs=60)
 
