894b3913f830a87baefb479faeccbb97d3093a10
Stefan Blumentrath <stefan.blumentrath@nina.no>
2021-09-06 23:22:09 +0200

    A and B

--- get_scenes_list.py
+++ get_scenes_list.py
@@ -9,19 +9,25 @@ directory = Path("./scene_lists")
 if not directory.exists():
     directory.mkdir()
 
-instr = os.environ["S2_NSTRUMENT"]
+#instr = os.environ["S2_NSTRUMENT"]
 proj = os.environ["S2_PROJ"]
-year = os.environ["S2_YEAR"]
+#year = os.environ["S2_YEAR"]
 
-print(f"Sentinel-2{instr}")
 print(f"{proj}")
 
 # DTERRENG data starts 2017
 for year in [2017, 2018, 2019, 2020, 2021]:
     print(f"{year}")
     scenes = gscript.read_command("m.crawl.thredds", 
-                              input=f"https://nbstds.met.no/thredds/catalog/NBS/S2{instr}/{year}/catalog.html",
-                              output=directory.joinpath(f"S2{instr}_{year}_{proj}.txt"),
+                              input=f"https://nbstds.met.no/thredds/catalog/NBS/S2A/{year}/catalog.html",
+                              output=directory.joinpath(f"S2A_{year}_{proj}.txt"),
                               filter=f".*{proj}.*DTERRENG.*",
-                              nprocs=200)
+                              nprocs=100,
+                              overwrite=True)
+    scenes = gscript.read_command("m.crawl.thredds", 
+                              input=f"https://nbstds.met.no/thredds/catalog/NBS/S2B/{year}/catalog.html",
+                              output=directory.joinpath(f"S2B_{year}_{proj}.txt"),
+                              filter=f".*{proj}.*DTERRENG.*",
+                              nprocs=100,
+                              overwrite=True)
 
