825bf6e5f3f1876a2569608dce7b2230554c6c13
Stefan Blumentrath <stefan.blumentrath@nina.no>
2021-09-06 09:16:34 +0200

    fix paths

--- get_scenes_list.py
+++ get_scenes_list.py
@@ -1,5 +1,20 @@
+#!/usr/bin/env python3
+
+from pathlib import Path
+import grass.script as gscript
+
+directory = Path("./scene_lists")
+
+if not directory.exists():
+    directory.mkdir()
+
 for instr in ["A", "B"]:
-    for proj in ["T29", "T30", "T31", "T32", "T33", "T34", "T35"]:
-        scenes = gscript.read_command("m.crawl.thredds", url="https://{}.xml".format(instr), filter=".*{}.*DTERRENG.*".format(proj), nprocs=10)
-        with open(f"S2{instr}_{proj}.txt", "a") as scene_list:
-            scene_list.write(scenes)
+    print(f"Sentinel-2{instr}")
+    for year in [2021]:
+        print(f"{year}")
+        for proj in ["T31", "T32", "T33", "T34", "T35"]:
+            print(f"{proj}")
+            scenes = gscript.read_command("m.crawl.thredds", url=f"https://nbstds.met.no/thredds/catalog/NBS/S2{instr}/{year}/catalog.html", filter=f".*{proj}.*DTERRENG.*", nprocs=10)
+            with open(directory.joinpaths(f"S2{instr}_{year}_{proj}.txt"), "w") as scene_list:
+                scene_list.write(scenes)
+
