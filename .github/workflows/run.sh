4ddeda7a8ce2d7c88bce08d8b02d44ef8062ae24
Stefan Blumentrath <stefan.blumentrath@nina.no>
2021-09-06 09:16:11 +0200

    fix paths

--- .github/workflows/run.sh
+++ .github/workflows/run.sh
@@ -5,13 +5,14 @@
 # git checkout https://github.com/OSGeo/grass
 # git checkout https://github.com/jhpoelen/zenodo-upload
 
+# "https://zenodo.org/api/files/311ffb11-4ac1-4b6f-8c06-abf6ec70962f/S2_demolocation.tar.gz" \
 grass --tmp-location XY --exec \
-    g.download.location url="https://zenodo.org/api/files/311ffb11-4ac1-4b6f-8c06-abf6ec70962f/S2_demolocation.tar.gz" \
+    g.download.location url="https://transfer.sh/J6K5fC/S2_demolocation.tar.gz" \
     path=.
 
 grass --tmp-location XY --exec python3 setup_grass.py
 
-grass --tmp-location XY --exec python3 list_nc.py
+grass --tmp-location XY --exec python3 get_scenes_list.py
 
 ls
 
