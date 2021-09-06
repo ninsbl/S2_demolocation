9c5a027f80418df99178fe7a5c7d80d7bf5f784e
Stefan Blumentrath <stefan.blumentrath@nina.no>
2021-09-06 09:54:34 +0200

    joinpath

--- build_strds.py
+++ build_strds.py
@@ -4,5 +4,12 @@ import grass.script as gscript
 
 gscript.run_command("g.region", res=10, flags="a")
 for instr in ["A", "B"]:
-    for proj in ["T31", "T32", "T33", "T34", "T35"]:
-        gscript.run_command("t.rast.import.netcdf", input=f"S2{instr}_{proj}.txt", output="Sentinel_2", bandref="bandref.txt", flags="la{}".format("o" if proj == "T33" else ""), nodata="-1,65535", nprocs=20)
+    for year in [2021]:
+        for proj in ["T31", "T32", "T33", "T34", "T35"]:
+            gscript.run_command("t.rast.import.netcdf",
+                                input=directory.joinpath(f"S2{instr}_{year}_{proj}.txt"),
+                                output="Sentinel_2",
+                                bandref="bandref.txt",
+                                flags="la{}".format("o" if proj == "T33" else ""),
+                                nodata="-1,65535",
+                                nprocs=20)
