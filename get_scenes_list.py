fcca8ed9ce81ba51a9db6ee255198ec06070bc15
Stefan Blumentrath <stefan.blumentrath@nina.no>
2021-09-30 09:49:37 +0200

    fewer cores both instruments

--- get_scenes_list.py
+++ get_scenes_list.py
@@ -11,24 +11,25 @@ if not directory.exists():
 
 instr = os.environ["S2_INSTRUMENT"]
 proj = os.environ["S2_PROJ"]
-#year = os.environ["S2_YEAR"]
+year = 2017
 
-print(f"{proj}")
-print(f"{instr}")
-print(f"{year}")
+for instr in ["A", "B"]:
+    print(f"{proj}")
+    print(f"{instr}")
+    print(f"{year}")
 
-# DTERRENG data starts 2017
-year = 2017 # for year in [2017, 2018, 2019, 2020, 2021]:
-    #scenes = gscript.read_command("m.crawl.thredds", 
-    #                          input=f"https://nbstds.met.no/thredds/catalog/NBS/S2A/{year}/catalog.html",
-    #                          output=directory.joinpath(f"S2A_{year}_{proj}.txt"),
-    #                          filter=f".*{proj}.*DTERRENG.*",
-    #                          nprocs=100,
-    #                          overwrite=True)
-scenes = gscript.read_command("m.crawl.thredds", 
-                              input=f"https://nbstds.met.no/thredds/catalog/NBS/S2B/{year}/catalog.html",
-                              output=directory.joinpath(f"S2{instr}_{year}_{proj}.txt"),
-                              filter=f".*{proj}.*DTERRENG.*",
-                              nprocs=30,
-                              overwrite=True)
+    # DTERRENG data starts 2017
+    # for year in [2017, 2018, 2019, 2020, 2021]:
+        #scenes = gscript.read_command("m.crawl.thredds", 
+        #                          input=f"https://nbstds.met.no/thredds/catalog/NBS/S2A/{year}/catalog.html",
+        #                          output=directory.joinpath(f"S2A_{year}_{proj}.txt"),
+        #                          filter=f".*{proj}.*DTERRENG.*",
+        #                          nprocs=100,
+        #                          overwrite=True)
+    scenes = gscript.read_command("m.crawl.thredds", 
+                                  input=f"https://nbstds.met.no/thredds/catalog/NBS/S2B/{year}/catalog.html",
+                                  output=directory.joinpath(f"S2{instr}_{year}_{proj}.txt"),
+                                  filter=f".*{proj}.*DTERRENG.*",
+                                  nprocs=50,
+                                  overwrite=True)
 
