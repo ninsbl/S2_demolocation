5c14e3e542e5884de5542ca9ad3fe24b3256cf2e
Stefan Blumentrath <stefan.blumentrath@nina.no>
2021-10-03 23:19:42 +0200

    uses fixes in crawler

--- get_scenes_list.py
+++ get_scenes_list.py
@@ -9,27 +9,15 @@ directory = Path("./scene_lists")
 if not directory.exists():
     directory.mkdir()
 
-instr = os.environ["S2_INSTRUMENT"]
-proj = os.environ["S2_PROJ"]
-year = 2017
-
 for instr in ["A", "B"]:
-    print(f"{proj}")
     print(f"{instr}")
-    print(f"{year}")
 
     # DTERRENG data starts 2017
-    # for year in [2017, 2018, 2019, 2020, 2021]:
-        #scenes = gscript.read_command("m.crawl.thredds", 
-        #                          input=f"https://nbstds.met.no/thredds/catalog/NBS/S2A/{year}/catalog.html",
-        #                          output=directory.joinpath(f"S2A_{year}_{proj}.txt"),
-        #                          filter=f".*{proj}.*DTERRENG.*",
-        #                          nprocs=100,
-        #                          overwrite=True)
-    scenes = gscript.read_command("m.crawl.thredds", 
+    for year in [2017, 2018, 2019, 2020, 2021]:
+        scenes = gscript.read_command("m.crawl.thredds", 
                                   input=f"https://nbstds.met.no/thredds/catalog/NBS/S2{instr}/{year}/catalog.html",
-                                  output=directory.joinpath(f"S2{instr}_{year}_{proj}.txt"),
-                                  filter=f".*{proj}.*DTERRENG.*",
+                                  output=directory.joinpath(f"S2{instr}_{year}.txt"),
+                                  filter=f".*DTERRENG.*",
                                   nprocs=50,
                                   overwrite=True)
 
