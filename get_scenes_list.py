88f098f5ca636c21694442906f424517718e5458
Stefan Blumentrath <stefan.blumentrath@gmx.de>
2021-10-04 22:09:15 +0200

    2020

--- get_scenes_list.py
+++ get_scenes_list.py
@@ -13,7 +13,7 @@ instr = os.environ["S2_INSTRUMENT"]
 print(f"{instr}")
 
 # DTERRENG data starts 2017
-for year in [2019, 2021]:
+for year in [2020]:
     print(f"{year}")
     for month in [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]:
         print(f"{month:02}")
