99fa55e86ec6593a1e8e91c8ded1b9f37fcc4332
Stefan Blumentrath <stefan.blumentrath@nina.no>
2021-10-05 14:02:07 +0200

    start linking

--- build_strds.py
+++ build_strds.py
@@ -1,15 +1,33 @@
 #!/usr/bin/env python3
 
+from pathlib import Path
+
 import grass.script as gscript
 
-gscript.run_command("g.region", res=10, flags="a")
+directory = Path("./scene_lists")
+
+tmp_file = Path("./tmp.txt")
+
+# Set region
 for instr in ["A", "B"]:
-    for year in [2021]:
+    for year in [2017]:
         for proj in ["T31", "T32", "T33", "T34", "T35"]:
-            gscript.run_command("t.rast.import.netcdf",
-                                input=directory.joinpath(f"S2{instr}_{year}_{proj}.txt"),
-                                output="Sentinel_2",
-                                bandref="bandref.txt",
-                                flags="la{}".format("o" if proj == "T33" else ""),
-                                nodata="-1,65535",
-                                nprocs=20)
+            for month in [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]:
+                infile = directory.joinpath(f"S2{instr}_{year}_{month}.txt")
+                if not infile.exists():
+                    continue
+
+                #filter, write to tmp file, use tmpfile as input
+                scenes = infile.read_text().split("\n")
+                rel_scenes = [scene for scene in scenes if f"_{proj}" in scene]
+                if rel_scenes:
+                    tmp_file.write(rel_scenes)
+                    # Import relevant scenes
+                    gscript.run_command("t.rast.import.netcdf",
+                                        input=tmp_file,
+                                        output="Sentinel_2_DTERRENG",
+                                        bandref="bandref.txt",
+                                        flags="la{}".format("o" if proj == "T33" else ""),
+                                        nodata="-1,65535",
+                                        nprocs=20)
+
